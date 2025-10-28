94bcdf9a96255aee405e4e923662ad4640e5616b
dokkiitech <info@dokkiitech.com>
2025-10-29 02:52:35 +0900

    いい感じに高さ変更

--- src/screens/links/LinkDetailScreen.tsx
+++ src/screens/links/LinkDetailScreen.tsx
@@ -606,7 +606,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
     justifyContent: 'flex-start',
     alignItems: 'flex-end',
-    paddingTop: 50,
+    paddingTop: 100,
     paddingRight: 10,
   },
   menuContainer: {
